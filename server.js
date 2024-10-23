b61243c6336dc69beac00a47e02b8c237e88baa0
Laura <laura.kask@tktk.ee>
2024-10-23 17:56:22 +0300

    Proov3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
 
 const server = http.createServer((request, response)=> {
